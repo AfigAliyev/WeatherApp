d4196de654ae2c83fb9dc9303a19844f7bdc5b3c
Maximillian Leonov <maximillianleonov@gmail.com>
2022-12-15 19:32:29 +0400

    Implement Dagger appComponent in MainApplication

--- app/src/main/kotlin/com/maximillianleonov/weatherapp/MainApplication.kt
+++ app/src/main/kotlin/com/maximillianleonov/weatherapp/MainApplication.kt
@@ -1,5 +1,10 @@
 package com.maximillianleonov.weatherapp
 
 import android.app.Application
+import com.maximillianleonov.weatherapp.di.AppComponent
+import com.maximillianleonov.weatherapp.di.DaggerAppComponent
+import com.maximillianleonov.weatherapp.util.unsafeLazy
 
-class MainApplication : Application()
+class MainApplication : Application() {
+    val appComponent: AppComponent by unsafeLazy { DaggerAppComponent.create() }
+}
