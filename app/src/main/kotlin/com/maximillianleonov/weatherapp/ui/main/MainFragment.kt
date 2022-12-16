09e7421ebab0db2f6f9b8992466ea277282d2908
Maximillian Leonov <maximillianleonov@gmail.com>
2022-12-16 09:45:32 +0400

    Add permission check to main screen

--- app/src/main/kotlin/com/maximillianleonov/weatherapp/ui/main/MainFragment.kt
+++ app/src/main/kotlin/com/maximillianleonov/weatherapp/ui/main/MainFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.maximillianleonov.weatherapp.R
 import com.maximillianleonov.weatherapp.databinding.FragmentMainBinding
+import com.maximillianleonov.weatherapp.util.PermissionUtils
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -18,6 +21,17 @@ class MainFragment : Fragment() {
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        if (!PermissionUtils.isPermissionGranted(requireContext())) {
+            findNavController().navigate(R.id.action_mainFragment_to_permissionFragment)
+        }
     }
 
     override fun onDestroyView() {
