581b8d7f562da38404e85fb86aff5893d631609f
Maximillian Leonov <maximillianleonov@gmail.com>
2022-12-16 14:44:04 +0400

    Implement application context provider

--- app/src/main/kotlin/com/maximillianleonov/weatherapp/MainApplication.kt
+++ app/src/main/kotlin/com/maximillianleonov/weatherapp/MainApplication.kt
@@ -2,9 +2,14 @@ package com.maximillianleonov.weatherapp
 
 import android.app.Application
 import com.maximillianleonov.weatherapp.di.AppComponent
+import com.maximillianleonov.weatherapp.di.AppModule
 import com.maximillianleonov.weatherapp.di.DaggerAppComponent
 import com.maximillianleonov.weatherapp.util.unsafeLazy
 
 class MainApplication : Application() {
-    val appComponent: AppComponent by unsafeLazy { DaggerAppComponent.create() }
+    val appComponent: AppComponent by unsafeLazy {
+        DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
 }
--- app/src/main/kotlin/com/maximillianleonov/weatherapp/di/AppModule.kt
+++ app/src/main/kotlin/com/maximillianleonov/weatherapp/di/AppModule.kt
@@ -1,6 +1,11 @@
 package com.maximillianleonov.weatherapp.di
 
+import android.content.Context
 import dagger.Module
+import dagger.Provides
 
-@Module
-interface AppModule
+@Module(includes = [DataModule::class])
+class AppModule(private val context: Context) {
+    @Provides
+    fun provideApplicationContext() = context
+}
